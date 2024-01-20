b59982ed3a0cf9d4bfe19a5b92741698fd13384a
Sowmya K S <119122284+Sowmya-K-S@users.noreply.github.com>
2024-01-20 19:01:40 +0530

    i am mad

---
+++ user/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+# Create your models here
+class User_Details(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    password = models.CharField(max_length=200)
+    points = models.IntegerField()    
+
+    def __str__(self):
+        return self.name
+
+# class User_History(models.Model):
+#     name = models.ForeignKey(User_Details, on_delete=models.CASCADE)
+#     date = models.DateField()
+#     food_emsn = models.IntegerField()
+#     travel_emsn= models.IntegerField()
+#     energy_emission = models.IntegerField()
+#     daily_emsn = models.IntegerField()
+
+#     def __str__ (self):
+#         return self.name
+    
+# class User_Goal(models.Model):
+#     name = models.ForeignKey(User_Details, on_delete=models.CASCADE)
+#     gl_travel_emsn = models.IntegerField()
+#     gl_energy_emsn = models.IntegerField()
+#     gl_food_emsn = models.IntegerField()
+#     gl_daily_emsn = models.IntegerField()
+
+#     def __str__ (self):
+#         return self.name
+
---
+++ user/views.py
@@ -0,0 +1,89 @@
+from django.shortcuts import render,redirect,HttpResponse
+from user.models import *
+from user.models import User_Details
+
+from random import randint
+from django.conf import settings
+from django.core.mail import send_mail
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def register(request):
+    global c_otp
+    # when we click on register option
+    if request.method == 'GET':
+        return render(request, 'register.html')
+    
+    # when we click on register button
+    else:
+       
+        # checking whether the entered email is already used for registration
+        try:
+            # error occurs when there is no email match
+            # then control goes to except block
+
+            User_Details.objects.get(email = request.POST['email'])
+            return render(request,'register.html',{'msg':"Email already registered, try using other Email"})
+        
+        except:
+            # validating password and confirm password
+            if request.POST['password'] == request.POST['cpassword']:
+                #  generating otp
+                global c_otp
+                c_otp = randint(100_000,999_999)
+
+                #extracting data from registration form
+                global reg_form_data 
+
+                reg_form_data = {
+                    "name" : request.POST['name'],
+                    "email" : request.POST['email'],
+                    "password" : request.POST['password'],
+                    "confirm_password" : request.POST['confirm_password']
+                    
+                }
+
+                # sending the generated OTP via mail
+                subject = "Medick Registration"
+                message = f'Hello{reg_form_data["full_name"]}, Welcome to Medick. Your OTP is {c_otp}'
+                sender = settings.EMAIL_HOST_USER
+                receiver = [reg_form_data['email']]
+                send_mail(subject, message, sender, receiver)
+
+                #after sending OTP render the page to enter OTP
+                return render(request,'otp.html')
+
+            else:
+                return render(request, 'register.html',{'msg':"Both Passwords didn't match"})
+            
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    else:
+        try:
+            # finding the record of person trying to login in database
+            session_user = User.objects.get(email = request.POST['email'])
+            if request.POST['password'] == session_user.password:
+                request.session['email'] = session_user.email
+                return redirect('home')
+            else:
+                return render(request, 'login.html', {'msg':'invalid password'})
+        except:
+            return render(request, 'login.html',{'msg': 'This email is not Registered !!'})
+
+def about(request):
+    return render(request,'about.html')
+
+def contact(request):
+    return render(request,'contact.html')
+
+def calculator(request):
+    return render(request,'calculator.html')
+
+def otp(request):
+    return render(request,'otp.html')
+
+def discover(request):
+    return render(request,'discover.html')
